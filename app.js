d21f2735e4cf4ba1aafcb8bc27f267ae18956923
Joaquin Bonaudi <jtbmelgar@gmail.com>
2024-06-01 16:00:46 +0200

    solved

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000
+
+app.use(express.static('public'));
+
+app.get('/', (request, response) => {
+    response.sendFile (__dirname + '/views/home.html')
+})
+
+app.get('/About', (request, response) => {
+    response.sendFile (__dirname + '/views/about.html')
+})
+
+app.get('/Works', (request, response) => {
+    response.sendFile (__dirname + '/views/works.html')
+})
+
+app.listen (port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
